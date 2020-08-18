8b2dd7722ebceb05a8680f933b9258fbd72dcee0
nicholas <web@zainaweb.com>
2020-08-18 19:24:00 +1000

    moding Command pattern

--- design-patterns-en/Ruby/src/structural_design_pattern/decorator/main.rb
+++ design-patterns-en/Ruby/src/structural_design_pattern/decorator/main.rb
@@ -80,9 +80,3 @@ decorator1 = ConcreteDecoratorA.new(simple)
 decorator2 = ConcreteDecoratorB.new(decorator1)
 puts 'Client: Now I\'ve got a decorated component:'
 client_code(decorator2)
-
-# Client: I've got a simple component:
-# RESULT: ConcreteComponent
-# 
-# Client: Now I've got a decorated component:
-# RESULT: ConcreteDecoratorB(ConcreteDecoratorA(ConcreteComponent))%
